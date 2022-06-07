807d8837a018e32d9ae682ed29140c052da31544
Diego Gielda <diego.gielda@GrupoAgros.local>
2022-06-06 22:49:45 -0300

    Finalizado o teste do link

--- cli.js
+++ cli.js
@@ -7,7 +7,7 @@ const caminho = process.argv;
 async function processaTexto(pCaminhoDoArquivo){
     const resultado = await pegaArquivo(pCaminhoDoArquivo[2]);
     if (pCaminhoDoArquivo[3] === 'validar'){
-        console.log(chalk.yellow('Links Validados'), validarURL(resultado));
+        console.log(chalk.yellow('Links Validados'), await validarURL(resultado));
     } else {
         console.log(chalk.yellow('Lista de Links'), resultado);
     }
--- http-validacao.js
+++ http-validacao.js
@@ -1,4 +1,22 @@
-function  geraArraydeURLs(pArrayLinks){
+import fetch from "node-fetch";
+
+function manejaErro(pErro) {
+    //throw new Error(pErro.message);
+}
+
+async function checaStatus(pArrayURLs) {
+    try {
+        const arrayStatus = await Promise.all(pArrayURLs.map(async url => {
+            const retorno = await fetch(url);
+            return retorno.status;
+        }));
+        return arrayStatus;
+    } catch (pErro) {
+        manejaErro(pErro);
+    }
+}
+
+function geraArraydeURLs(pArrayLinks) {
     // loop para cada { chave: valor }
     // objeto -> [valor]
     // Object.Values(pObjeto)
@@ -6,7 +24,10 @@ function  geraArraydeURLs(pArrayLinks){
 }
 
 
-export default function validarURL(pArrayLinks) {
-    return geraArraydeURLs(pArrayLinks);
-}
+export default async function validarURL(pArrayLinks) {
+    const links = geraArraydeURLs(pArrayLinks);
+    const statusLinks = await checaStatus(links);
+    const resultado = pArrayLinks.map((objeto, indice) => ({ ...objeto, status: statusLinks[indice] }));
 
+    return resultado;
+}
